94bb199a067581e50e4cd5ff68c30d45373e5032
HEMASANKAR G <124870580+sankar987@users.noreply.github.com>
2024-11-03 20:26:25 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,41 @@
+var a=10
+var b=20
+function add(){
+    console.log(a+b)
+}
+
+add()
+
+var factor='vijay'
+var fplayer='dhoni'
+var fmovie='anbe sivam'
+function favourite(){
+
+console.log("Favourite Actor:"+factor)
+console.log("Favourite Player:"+fplayer)
+console.log("Favourite Movie:"+fmovie)
+}
+
+favourite()
+
+function area(len,bre)
+{
+    var a=len*bre
+    console.log("Area is:"+a)
+}
+area(10,20)
+
+function myname()
+{
+    return "Hemasankar"
+}
+
+var m=myname()
+console.log(m)
+
+function add(x,y)
+{
+    return x+y
+}
+var total=add(30,20)
+console.log(total)
