135df8abaae638bd1d22294918c00501428fdea2
HEMASANKAR G <124870580+sankar987@users.noreply.github.com>
2024-11-04 17:15:55 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,15 @@
+for(count=1;count<=10;count=count+1){
+    console.log(count)
+}
+
+for(i=1;i<=10;i=i+2){
+    console.log(i)
+}
+
+for(j=10;j>=1;j=j-1){
+    console.log(j)
+}
+
+for(t=1;t<=10;t=t+1){
+    console.log(t+"x2="+t*2)
+}
